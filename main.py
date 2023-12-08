b8c238659e340237b221dcb12cea1d3ec53d7055
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 16:23:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class_list = {'0': 'Normal', '1': 'Pneumonia'}
 
-st.title('Pneumonia Detection')
+st.title('Pneumonia prediction based on chest X-Ray image')
 
 input = open('lrc_xray.pkl','rb')
 model = pkl.load(input)
